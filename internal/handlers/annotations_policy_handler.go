1f52ec979d428b783d0611857ea288e782bffd4e
pohanhuangtw <pohan.huang@suse.com>
2025-08-14 16:46:25 +0800

    f
    Signed-off-by: pohanhuangtw <pohan.huang@suse.com>

---
+++ internal/handlers/annotations_policy_handler.go
@@ -0,0 +1,91 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/neuvector/neuvector-kubewarden-policy-converter/internal/share"
+	nvapis "github.com/neuvector/neuvector/controller/api"
+	nvdata "github.com/neuvector/neuvector/share"
+)
+
+type HostNamespaceHandler struct {
+	BasePolicyHandler
+}
+
+const (
+	PolicySettingAllowHostIPC     = "allow_host_ipc"
+	PolicySettingAllowHostNetwork = "allow_host_network"
+	PolicySettingAllowHostPID     = "allow_host_pid"
+
+	RuleAnnotations = "annotations"
+)
+
+func NewHostNamespaceHandler() *HostNamespaceHandler {
+	return &HostNamespaceHandler{
+		BasePolicyHandler: BasePolicyHandler{
+			Unsupported:  false,
+			SupportedOps: map[string]bool{nvdata.CriteriaOpEqual: true},
+			Name:         share.ExtractModuleName(share.PolicyHostNamespacesPSPURI),
+			Module:       share.PolicyHostNamespacesPSPURI,
+		},
+	}
+}
+
+func (h *HostNamespaceHandler) getBoolValue(value string) (bool, error) {
+	boolValue, err := strconv.ParseBool(value)
+	if err != nil {
+		return false, err
+	}
+
+	// NeuVector uses the opposite of the boolean value.
+	// For example, if the value is "true", the policy will be disabled.
+	// If the value is "false", the policy will be enabled.
+	return !boolValue, nil
+}
+
+// BuildPolicySettings builds settings for a single criterion.
+func (h *HostNamespaceHandler) BuildPolicySettings(criterion *nvapis.RESTAdmRuleCriterion) ([]byte, error) {
+	return h.BuildGroupedPolicySettings([]*nvapis.RESTAdmRuleCriterion{criterion})
+}
+
+// BuildGroupedPolicySettings builds settings from multiple criteria that map to the same module
+// This allows combining shareIPC, shareNetwork, and sharePID rules into a single policy.
+func (h *HostNamespaceHandler) BuildGroupedPolicySettings(criteria []*nvapis.RESTAdmRuleCriterion) ([]byte, error) {
+	settings := map[string]bool{
+		PolicySettingAllowHostIPC:     true,
+		PolicySettingAllowHostPID:     true,
+		PolicySettingAllowHostNetwork: true,
+	}
+
+	// Process each criterion and set the appropriate flag
+	var err error
+	for _, criterion := range criteria {
+		switch criterion.Name {
+		case RuleShareIPC:
+			settings[PolicySettingAllowHostIPC], err = h.getBoolValue(criterion.Value)
+			if err != nil {
+				return nil, err
+			}
+		case RuleShareNetwork:
+			settings[PolicySettingAllowHostNetwork], err = h.getBoolValue(criterion.Value)
+			if err != nil {
+				return nil, err
+			}
+		case RuleSharePID:
+			settings[PolicySettingAllowHostPID], err = h.getBoolValue(criterion.Value)
+			if err != nil {
+				return nil, err
+			}
+		default:
+			return nil, fmt.Errorf("unsupported criterion: %s", criterion.Name)
+		}
+	}
+
+	settingsBytes, err := json.Marshal(settings)
+	if err != nil {
+		return nil, err
+	}
+	return settingsBytes, nil
+}
