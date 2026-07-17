0e53f56d0bbfe9155aa99791c232d3304b536c52
Pohan Huang <pohan.huang@suse.com>
2026-07-17 09:46:16 +0800

    chore: commitlint ignore the commit by dependenbot
    Signed-off-by: pohanhuang <pohan.huang@suse.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
+    ignores: [(commit) => commit.includes("Signed-off-by: dependabot[bot]")],
     rules: {
       "body-max-line-length": [0, "always", Infinity], // disables the 100-char limit
       "header-max-length": [0, "always", Infinity], // disables the 100-char limit
